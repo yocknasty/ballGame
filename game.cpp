a4e905e3e2771e6fd64a30077482fb389f2ec202
yocknasty <ajyockey@gmail.com>
2018-02-17 21:05:28 -0800

    ball moves!

--- game.cpp
+++ game.cpp
@@ -1,4 +1,6 @@
 #include "game.h"
+const sf::Time TimePerFrame = sf::seconds(1.f/ 60.f);
+const float PlayerSpeed = 100.f; 
 
 Game::Game()
     :mWindow(sf::VideoMode(640, 480), "ball game"), mPlayer()
@@ -6,29 +8,69 @@ Game::Game()
         mPlayer.setRadius(40.f);
         mPlayer.setPosition(100.f, 100.f);
         mPlayer.setFillColor(sf::Color::Red);
+        bool isMovingUp(false);
+        bool isMovingDown(false);
+        bool isMovingLeft(false);
+        bool isMovingRight(false);
     }
 
 void Game::run(){
+    sf::Clock clock;
+    sf::Time timeSinceUpdate = sf::Time::Zero;
     while (mWindow.isOpen()){
-        processEvents();
-        update();
+        sf::Time elapsedTime = clock.restart();
+        timeSinceUpdate += elapsedTime;
+        
+        while (timeSinceUpdate > TimePerFrame){
+            timeSinceUpdate -= TimePerFrame;
+            processEvents();
+            update(TimePerFrame);
+        }
         render();
     }
+}
+void Game::handlePlayerInput(sf::Keyboard::Key key, bool isPressed){
+    if(key== sf::Keyboard::W)
+        isMovingUp = isPressed;
+    else if (key==sf::Keyboard::S)
+        isMovingDown = isPressed;
+    else if (key==sf::Keyboard::A)
+        isMovingLeft = isPressed;
+    else if (key==sf::Keyboard::D)
+        isMovingRight = isPressed;
 }
 
 void Game::processEvents(){
     sf::Event event;
     while (mWindow.pollEvent(event)){
         switch (event.type){
-            
+            case sf::Event::KeyPressed:
+                handlePlayerInput(event.key.code, true);
+                break;
+
+            case sf::Event::KeyReleased:
+                handlePlayerInput(event.key.code, false);
+                break;
+
+            case sf::Event::Closed:
+                mWindow.close();
+                break;
         }
-        if(event.type == sf::Event::Closed)
-            mWindow.close();
     }
 }
 
-void Game::update(){
-
+void Game::update(sf::Time deltaTime){
+    sf::Vector2f movement(0.f, 0.f);
+    if(isMovingUp)
+        movement.y -= PlayerSpeed;
+    if(isMovingDown)
+        movement.y += PlayerSpeed;
+    if(isMovingLeft)
+        movement.x -= PlayerSpeed;
+    if(isMovingRight)
+        movement.x += PlayerSpeed;
+    
+    mPlayer.move(movement * deltaTime.asSeconds());
 }
 
 void Game::render(){
--- game.h
+++ game.h
@@ -10,12 +10,16 @@ class Game
     
     private:
         void processEvents();
-        void update();
+        void update(sf::Time deltaTime);
         void render();
-    
+        void handlePlayerInput(sf::Keyboard::Key key, bool isPressed);
     private:
+        
         sf::RenderWindow mWindow;
         sf::CircleShape mPlayer;
+        
+        
+
 };
 
 #endif // BALL_GAME_H
